69a55ffba430d8ad72e739b909dd8519c05ccdaa
TheGreenCoconut <quinn.harris@icloud.com>
2026-09-22 09:01:45 -0700

    (GP-2.3): Create BLOB Files

--- Git.java
+++ Git.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.util.HexFormat;
@@ -8,14 +9,19 @@ public class Git {
     public static void main(String[] args) {
         init();
 
-        System.out.println(hashFile("text.md"));
+        save("text.md");
     }
 
-    public static void init() {
+    /*
+     * Initializes the repository's necessary files. Creates the git directory with children
+     * objects/, HEAD and index. Returns true if the repository could be created, returns false if
+     * not or if the current repo already exists
+     */
+    public static boolean init() {
         File git = new File("./git/");
         if (!git.mkdir()) {
             System.out.println("Git Repository Already Exists");
-            return;
+            return false;
         }
 
         File objects = new File("./git/objects/");
@@ -25,15 +31,20 @@ public class Git {
         try {
             if (!objects.mkdir() || !head.createNewFile() || !index.createNewFile()) {
                 System.out.println("Git Repository Already Exists");
-                return;
+                return false;
             }
         } catch (IOException e) {
             System.out.println("Failed to create Git Repository");
+            return false;
         }
 
         System.out.println("Git Repository Created at " + git.getParentFile().getAbsolutePath());
+        return true;
     }
 
+    /*
+     * Hashes a file based on its contents. Returns a SHA-1 hash unique to this file's contents.
+     */
     public static String hashFile(String filePath) {
         try {
             StringBuilder contents = new StringBuilder();
@@ -52,5 +63,29 @@ public class Git {
             return null;
         }
 
+    }
+
+    /*
+     * Creates a blob file for the given filepath and stores it inside the objects folder. Returns
+     * true if the file could be saved to objects, returns false otherwise.
+     */
+    public static boolean save(String filePath) {
+        try {
+            File blob = new File("./git/objects/" + hashFile(filePath));
+            FileReader reader = new FileReader(filePath);
+            FileWriter writer = new FileWriter(blob);
+            int c;
+            while ((c = reader.read()) != -1) {
+                writer.write((char) (c));
+            }
+            reader.close();
+            writer.close();
+
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Failed to save changes to file at " + filePath);
+            return false;
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # fogitaboutit
+
 Honors Topics Git Recreation
 
-GP-2.1 — Initialize a Repository - init is now functional, creates full setup for 
+GP-2.1: Initialize a Repository - init() is now functional, creates full setup for
 repo initialization
 
-GP-2.2 — Create a Hash Function - hash function implemented, files can now be 
+GP-2.2: Create a Hash Function - hashFile() function implemented, files can now be
 hashed for storing in objects (unimplemented)
+
+(GP-2.3): Create BLOB Files - save() function implemented, hashes a file and stores it as a BLOB inside the objects folder;
+index unimplemented
